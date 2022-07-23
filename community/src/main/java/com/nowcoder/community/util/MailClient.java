9e6130769bed7b8f927c8b1826d1a81817a24744
Auroragys <auroragys@gmail.com>
2022-07-23 19:42:14 +0800

    community Home complete

---
+++ community/src/main/java/com/nowcoder/community/util/MailClient.java
@@ -0,0 +1,26 @@
+/*
+package com.nowcoder.community.util;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import java.time.chrono.MinguoEra;
+import java.util.logging.Logger;
+
+@Component
+public class MailClient {
+    private static final Logger logger= (Logger) LoggerFactory.getLogger(MailClient.class);
+    @Autowired
+    private JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void setMail(String to,String subject,String,String content){
+        MimeMessage message=mailSender.create
+    }
+
+}
+*/
